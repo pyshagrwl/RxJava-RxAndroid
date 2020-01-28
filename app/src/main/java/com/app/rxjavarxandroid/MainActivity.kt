99a5303f7986988ae0444f255ee4ca7bd3dd2169
piyagarw1 <piyagarw1@publicisgroupe.net>
2020-01-28 12:26:25 +0530

    start from zero

--- app/src/main/java/com/app/rxjavarxandroid/MainActivity.kt
+++ app/src/main/java/com/app/rxjavarxandroid/MainActivity.kt
@@ -1,13 +1,49 @@
 package com.app.rxjavarxandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var observer: Observer<String>
+    val greetings = "Welcome to RxJava"
+    lateinit var myObservable: Observable<String>
+    val TAG = "MyActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myObservable = Observable.just(greetings)
+        observer = object : Observer<String> {
+            override fun onComplete() {
+                Log.e(TAG, "onComplete method called")
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                Log.e(TAG, "onSubscribe method called")
+
+            }
+
+            override fun onNext(t: String) {
+                Log.e(TAG, "onNext method called")
+
+                txtvw.setText(t)
+            }
+
+            override fun onError(e: Throwable) {
+                Log.e(TAG, "onError method called")
+
+            }
+
+        }
+        myObservable.subscribe(observer)
+
+
     }
 
 }
