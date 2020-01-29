9eab7ec0efcf541d8be66a351310989de4115bde
piyagarw1 <piyagarw1@publicisgroupe.net>
2020-01-29 12:57:45 +0530

    create operator

--- app/src/main/java/com/app/rxjavarxandroid/MainActivity.kt
+++ app/src/main/java/com/app/rxjavarxandroid/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.app.rxjavarxandroid
 
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
+import java.time.Instant
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var observer: DisposableObserver<Int>
+    private lateinit var observer: DisposableObserver<Student>
     private val TAG = "MyActivity"
 
-    private lateinit var myObservableJustWithArray: Observable<Int>
+    private lateinit var myObservableJustWithArray: Observable<Student>
 
     private var compositeDisposable = CompositeDisposable()
 
@@ -25,7 +32,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        myObservableJustWithArray = Observable.range(20,40)
+        myObservableJustWithArray = Observable.create(object : ObservableOnSubscribe<Student> {
+            override fun subscribe(emitter: ObservableEmitter<Student>) {
+                val students = getStudents()
+                for (student in students) {
+                    emitter.onNext(student)
+                }
+                emitter.onComplete()
+            }
+
+        })
 
         compositeDisposable.add(
             myObservableJustWithArray
@@ -36,13 +52,13 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun getArrayObserver(): DisposableObserver<Int> {
-        observer = object : DisposableObserver<Int>() {
+    private fun getArrayObserver(): DisposableObserver<Student> {
+        observer = object : DisposableObserver<Student>() {
             override fun onComplete() {
                 Log.e(TAG, "onComplete method called")
             }
 
-            override fun onNext(t: Int) {
+            override fun onNext(t: Student) {
                 Log.e(TAG, "onNext method called: $t")
 
             }
@@ -61,6 +77,19 @@ class MainActivity : AppCompatActivity() {
         compositeDisposable.clear()
     }
 
+
+    @TargetApi(Build.VERSION_CODES.O)
+    fun getStudents(): ArrayList<Student> {
+        val list = ArrayList<Student>()
+        list.add(Student("Student1", "student1@gmail.com", 20, Date.from(Instant.now())))
+        list.add(Student("Student2", "student2@gmail.com", 22, Date.from(Instant.now())))
+        list.add(Student("Student3", "student3@gmail.com", 19, Date.from(Instant.now())))
+        list.add(Student("Student4", "student4@gmail.com", 22, Date.from(Instant.now())))
+        list.add(Student("Student5", "student5@gmail.com", 21, Date.from(Instant.now())))
+        list.add(Student("Student6", "student6@gmail.com", 20, Date.from(Instant.now())))
+
+        return list
+    }
 
 }
 
---
+++ app/src/main/java/com/app/rxjavarxandroid/Student.kt
@@ -0,0 +1,5 @@
+package com.app.rxjavarxandroid
+
+import java.util.*
+
+data class Student(var name: String, var email: String, var age: Int, var registrationDate: Date)
